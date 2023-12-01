d8ff63ebb9c9ef74f05e72d6f9c102b1bbbbb998
Alexlikhanov2011 <Likhanov-2011@yandex.ru>
2023-12-01 19:29:38 +0300

    Бот завершен.

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -5,19 +5,44 @@ import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.component.SendHelper;
+import pro.sky.telegrambot.service.NotificationTaskService;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.logging.Level.parse;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final static String WELCOME_TEXT =
+            "Привет! Это бот-планирования:)! Для планирования задачи отправьте её в формате: 01.01.2022 20:00 Сделать домашнюю работу";
 
-    @Autowired
-    private TelegramBot telegramBot;
+    private static final Pattern PATTERN = Pattern.compile("([0-9.:\\s]{16})(\\s)([\\W+]+)");
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+
+    private final TelegramBot telegramBot;
+    private final NotificationTaskService notificationTaskService;
+    private  final SendHelper sendHelper;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot,
+                                      NotificationTaskService notificationTaskService,
+                                      SendHelper sendHelper) {
+        this.telegramBot = telegramBot;
+        this.notificationTaskService = notificationTaskService;
+        this.sendHelper = sendHelper;
+    }
 
     @PostConstruct
     public void init() {
@@ -26,11 +51,36 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Override
     public int process(List<Update> updates) {
-        updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            // Process your updates here
-        });
+        try {
+            updates.forEach(update -> {
+                String text = update.message().text();
+                Long chatId = update.message().chat().id();
+                if ("/start".equals(text)) {
+                    sendHelper.sendMessage (chatId, WELCOME_TEXT);
+                } else {
+                    Matcher matcher = PATTERN.matcher(text);
+                    LocalDateTime localDateTime;
+                    if (matcher.find() &&  (localDateTime = parse(matcher.group(1))) !=null) {
+                        String message = matcher.group(3);
+                        notificationTaskService.create(chatId, message, localDateTime);
+                        sendHelper.sendMessage(chatId, "Задача запланирована");
+                    } else {
+                        sendHelper.sendMessage(chatId, "Некорректный формат сообщения!");
+                    }
+                }
+            });
+        }catch (Exception e) {
+            logger.error(e.getMessage(),e);
+        }
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+    @Nullable
+    private LocalDateTime parse(String localDateTime){
+        try {
+            return LocalDateTime.parse(localDateTime, FORMATTER);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
     }
 
 }
